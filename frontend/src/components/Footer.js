6aec07578aa7a1d7265d0aa2d4596a8633e77ef4
Daniell Delgado <dpdelgado@up.edu.ph>
2022-03-11 13:49:36 +0800

    edit footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,16 +2,15 @@ import React from 'react'
 
 function Footer(){
     return(
-        <div className="mainfooter">
+        <div className="footer">
             <div className="topfooter">
-                <div className="column1">
-                    <h3>Map</h3>
+                <div className="column">
                     <iframe className="map" allowFullScreen 
                     title="Map of The Fisherman's Cafe"
                     src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA6KFP8N5jSy6JfjxYXPL2t5iZCqSqx2yw&q=place_id:ChIJYxm5QKGJqDMRLdh_A0crcdI&zoom=17&region=ph">
                     </iframe>
                 </div>
-                <div className="column2">
+                <div className="column">
                     <h3>Cafe Hours</h3>
                     <h5>Mondays to Sundays.</h5>
                     <p>9:00 AM - 7:00 PM</p>
@@ -19,10 +18,10 @@ function Footer(){
                     <p>St. Peter and Paul Parish, Binaobao,</p>
                     <p>Bantayan Island</p>
                 </div>
-                <div className="Column3">
+                <div className="column">
                     <h3>Contact</h3>
-                    <a className="navigation-link" href="#facebook">facebook.com/thefishermanscafe</a><br/><br/>
-                    <a className="navigation-link" href="#messenger">m.me/thefishermanscafe</a>
+                    <a className="fab fa-facebook-f" href="#facebook"> /thefishermanscafe</a><br/><br/>
+                    <a className="fa-solid fa-comment" href="#messenger"> /thefishermanscafe</a>
                     <h3>Others</h3>
                     <a className="navigation-link" href="#faq">Frequentyly Asked Questions (FAQs)</a><br/><br/>
                     <a className="navigation-link" href="#partners">For Partners</a>
