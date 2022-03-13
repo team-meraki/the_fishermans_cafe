26415b6004290f98c4ab6a1b64abf853e3b8114f
Daniell Delgado <dpdelgado@up.edu.ph>
2022-03-13 21:25:40 +0800

    edit slider

--- frontend/src/components/Promotional.js
+++ frontend/src/components/Promotional.js
@@ -3,22 +3,22 @@ import {Carousel} from 'react-bootstrap'
 
 const Promotional = () => {
     return (
-        <Carousel className= 'carousel' variant='dark'>
-            <Carousel.Item interval={2000}>
+        <Carousel className='slider' variant='dark' fade indicators={false}>
+            <Carousel.Item interval={5000}>
                 <img
                     className="d-block w-100"
                     src="/images/1.jpeg"
                     alt="First slide"
                 />
             </Carousel.Item>
-            <Carousel.Item interval={2000}>
+            <Carousel.Item interval={5000}>
                 <img
                     className="d-block w-100"
                     src="/images/2.jpg"
                     alt="Second slide"
                 />
             </Carousel.Item>
-            <Carousel.Item interval={2000}>
+            <Carousel.Item interval={5000}>
                 <img
                     className="d-block w-100"
                     src="/images/3.jpg"
