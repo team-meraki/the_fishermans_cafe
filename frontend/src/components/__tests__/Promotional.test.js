dd3eedc76df5b72ecc86d8ee08f3fe5f055f0753
Daniell Delgado <dpdelgado@up.edu.ph>
2022-04-07 16:38:26 +0800

    add floating order button

--- frontend/src/components/__tests__/Promotional.test.js
+++ frontend/src/components/__tests__/Promotional.test.js
@@ -6,8 +6,8 @@ it('renders promotional buttons', () => {
     render(<Promotional />);
     const prev = screen.getByRole('button', {  name: /previous/i});
     expect(prev).toBeInTheDocument();
-    const order = screen.getAllByRole('button', {  name: /order/i});
-    order.forEach(viewElement => {
+    const viewProduct = screen.getAllByRole('button', {  name: /view product/i});
+    viewProduct.forEach(viewElement => {
         expect(viewElement).toBeInTheDocument();
     });
     const next = screen.getByRole('button', {  name: /next/i});
