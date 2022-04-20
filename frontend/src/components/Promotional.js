e04fc8380960085e23a4a06800e3523979b1324d
Sheila <sheila.craus@module-zero.com>
2022-04-20 13:46:10 +0800

    view full menu button

--- frontend/src/components/Promotional.js
+++ frontend/src/components/Promotional.js
@@ -1,31 +1,84 @@
 import React from 'react'
-import {Container, Button} from 'react-bootstrap'
-
-// icons
-import arrowRightWhiteIcon from '../icons/arrow-right-white.png'
-
-// styles
+import {Carousel, Container, Row, Col, Button} from 'react-bootstrap'
 import '../styles/Promotional.scss';
 
 const Promotional = () => {
     return (
-        <Container fluid className='promo-section d-flex flex-column justify-content-center align-items-center'>
-            <div className='d-flex flex-column justify-content-center align-items-center'>
-                <h1 className='promo-header'>
-                    Restaurant and Caf&#233; in the Island
-                </h1>
-                <h5 className='promo-subheader'>
-                    meals, desserts, and drinks
-                </h5>
-            </div>
+        <Container fluid className='promo-section'>
+            <Row className='brand-name-wrapper'>
+                <img
+                    alt=""
+                    src="/images/brand-name-white.svg"
+                    className="brand-name-img"
+                    />{' '}
+            </Row>
+            
+            {/* 
+            <Row>
+                <Col className="recom-section-content">
+                    <h1>Rolled to perfection</h1>
+                    <h5>Brazo de Mercedes</h5>
+                    <br/>
+                    <p>
+                    Rolls of our irresistible Brazo de Mercedes are rolling out for you!
+                    </p>
+                    
+                    <Button href='#product' className='recom-btn .btn-primary'>View product</Button>
+                </Col>
+            </Row>
+            */}
 
-            <div>
-                <Button className='promo-btn' href='/menu' size="sm"> 
-                    <span>View full menu</span>
-                    <img src={arrowRightWhiteIcon}></img>
-                </Button>
-            </div>
-        
+        {/* 
+        <Carousel indicators={false} controls={false} className= 'd-block carousel' variant='dark'>
+            <Carousel.Item interval={5000}>
+                <Container fluid className="recom-section">
+                    <Row>
+                        <Col className="recom-section-img">
+                        <img
+                            alt=""
+                            src="/images/recommended_product.png"
+                            className="recom-product"
+                            />
+                        </Col>
+                        <Col className="recom-section-content">
+                            <h1>Rolled to perfection</h1>
+                            <h5>Brazo de Mercedes</h5>
+                            <br/>
+                            <p>
+                            Rolls of our irresistible Brazo de Mercedes are rolling out for you!
+                            </p>
+                            
+                            <Button href='#product' className='recom-btn .btn-primary'>View product</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </Carousel.Item>
+            
+            <Carousel.Item interval={5000}>
+                <Container fluid className="recom-section">
+                    <Row>
+                        <Col className="recom-section-img">
+                        <img
+                            alt=""
+                            src="/images/teriyaki.jpg"
+                            className="recom-product"
+                            />
+                        </Col>
+                        <Col className="recom-section-content">
+                            <h1>Sizzling Chicken</h1>
+                            <h5>Japanese Chicken Teriyaki Rice Meal</h5>
+                            <br/>
+                            <p>
+                            We use real Japanese rice wine for that sweet and tangy taste!
+                            </p>
+                            
+                            <Button href='#product' className='recom-btn .btn-primary'>View product</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </Carousel.Item>
+        </Carousel>
+        */}
 
         </Container>
     )
