bac247500947ab7a74ad486950c9317d97357754
Edsel Codoy <escodoy@up.edu.ph>
2022-04-23 14:06:09 +0800

    removing interior/exterior button from test

--- frontend/src/components/__tests__/Gallery.test.js
+++ frontend/src/components/__tests__/Gallery.test.js
@@ -5,9 +5,3 @@ it('renders gallery heading', () => {
     render(<Gallery />)
     expect(screen.getByRole('heading', { name: /Explore our space/i })).toBeInTheDocument()
 })
-
-it('renders inside/outside buttons', () => {
-    render(<Gallery />)
-    expect(screen.getByRole('button', { name: /inside/i })).toBeInTheDocument()
-    expect(screen.getByRole('button', { name: /outside/i })).toBeInTheDocument()
-})
