57445214421c6e5ef2e1da58c53c7c17f342de4c
Edsel Codoy <escodoy@up.edu.ph>
2022-04-24 20:49:13 +0800

    fix merge conflict

---
+++ frontend/src/components/menu/Menu.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import { Container, Tab, Tabs } from 'react-bootstrap'
+import ProductsDisplay from './ProductsDisplay'
+
+import '../../styles/Menu.scss';
+
+export default function Menu() {
+  
+    // Get all products from the database
+    let [products, setProduct] = useState([]);
+
+    useEffect(() => {
+        let mounted = true
+        getProduct()
+        .then(products => {
+            if(mounted) {
+                setProduct(products)
+            }
+        })
+        return () => mounted = false
+    }, [])
+
+    let getProduct = async () => {
+        let response = await fetch("/api/product/")
+        let data = await response.json()
+        return data
+    }
+
+    // Filter products the belong to meal catgory
+    const meals = products.filter(product => product.category === 'meal');
+
+    // Filter products the belong to dessert catgory
+    const desserts = products.filter(product => product.category === 'dessert');
+
+    // Filter products the belong to drink catgory
+    const drinks = products.filter(product => product.category === 'drink');
+
+
+    return (
+      <Container className='menu-wrapper'>
+        <h1 className='menu-title'>Our Menu</h1>
+        <Container className='menu-tab d-flex justify-content-center flex-column'>
+          <Tabs defaultActiveKey="meal">
+            <Tab eventKey="meal" title="Meals">
+              <ProductsDisplay products={meals} />
+            </Tab>
+            <Tab eventKey="desssert" title="Desserts">
+              <ProductsDisplay products={desserts} />
+            </Tab>
+            <Tab eventKey="drink" title="Drinks">
+              <ProductsDisplay products={drinks} />
+            </Tab>
+          </Tabs>
+        </Container>
+      </Container>
+    )
+  
+}
