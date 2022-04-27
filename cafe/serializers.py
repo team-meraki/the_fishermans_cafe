84662009bdd63a0fd308f416f8f9763f494c101a
Edsel Codoy <escodoy@up.edu.ph>
2022-04-27 12:27:30 +0800

    worked on user views and JWT authentication

--- cafe/serializers.py
+++ cafe/serializers.py
@@ -1,35 +1,132 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Product, Testimonial, CafeInfo, Gallery, About, FeaturedProduct
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth import password_validation
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-class ProductSerializer(ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
-class TestimonialSerializer(ModelSerializer):
+class TestimonialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Testimonial
         fields = '__all__'
 
-class CafeInfoSerializer(ModelSerializer):
+class CafeInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = CafeInfo
         fields = '__all__'
 
-class GallerySerializer(ModelSerializer):
+class GallerySerializer(serializers.ModelSerializer):
     class Meta:
         model = Gallery
         fields = '__all__'
 
-class AboutSerializer(ModelSerializer):
+class AboutSerializer(serializers.ModelSerializer):
     class Meta:
         model = About
         fields = '__all__'
 
-class FeaturedProductSerializer(ModelSerializer):
-    product_id = ProductSerializer()
+class FeaturedProductSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(source='product_id', read_only=True)
     class Meta:
         model =  FeaturedProduct
-        fields = '__all__'
+        fields = ('id', 'product_id', 'product')
 
+class RegisterUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    password = serializers.CharField(
+            write_only=True, required=True, 
+            validators=[password_validation.validate_password]
+            )
+    confirmed_password = serializers.CharField(write_only=True, required=True)
+    
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'confirmed_password')
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirmed_password']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
 
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        
+        user.set_password(validated_data['password'])
+        user.save()
+        
+        return user
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+    confirmed_password = serializers.CharField(write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError(
+                {'old_password': 'The old password is incorrect.'}
+            )
+        return value
+
+    def validate(self, data):
+        if data.get('new_password') != data.get('confirmed_password'):
+            raise serializers.ValidationError({'new_password': "The two password fields didn't match."})
+        password_validation.validate_password(data.get('new_password'), self.context['request'].user)
+        return data
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['new_password'])
+        instance.save()
+        return instance
+
+class ChangeUsernameOrEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    def validate_email(self, value):
+        user = self.context['request'].user
+        if User.objects.exclude(pk=user.pk).filter(email=value).exists():
+            raise serializers.ValidationError({"email": "This email is already in use."})
+        return value
+
+    def validate_username(self, value):
+        user = self.context['request'].user
+        if User.objects.exclude(pk=user.pk).filter(username=value).exists():
+            raise serializers.ValidationError({"username": "This username is already in use."})
+        return value
+    
+    def validate_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError(
+                {'password': 'The password is incorrect.'}
+            )
+        return value
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.username = validated_data.get('username', instance.username)
+        instance.save()
+
+        return instance
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        token['username'] = user.username
+
+        return token
--- the_fishermans_cafe/settings.py
+++ the_fishermans_cafe/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "cafe.apps.CafeConfig",
     "rest_framework",
     "corsheaders",
+    'rest_framework_simplejwt.token_blacklist',
     "django_cleanup.apps.CleanupConfig",
 ]
 
@@ -130,6 +131,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'cafe/media/')
 MEDIA_URL = 'media/'
@@ -144,3 +153,35 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'username',
+    'USER_ID_CLAIM': 'username',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
