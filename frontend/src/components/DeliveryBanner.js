ece63e63fa7d73b7a0e60be39a8bc00364ab6d14
scraus <sccraus@up.edu.ph>
2022-06-03 21:44:03 +0800

    added unpost/post btns & fixed settings css

--- frontend/src/components/DeliveryBanner.js
+++ frontend/src/components/DeliveryBanner.js
@@ -4,7 +4,7 @@ import '../styles/DeliveryBanner.scss';
 
 const DeliverBanner = () => {
     return (
-        <Container fluid className='deliverybanner-section d-flex flex-column justify-content-center align-items-center'>
+        <Container fluid className='deliverybanner-section py-0 my-0'>
             <div className='d-flex flex-column justify-content-center align-items-center'>
 
             </div>
